9c5026d53972ef96a6635efc52f6eb0186bf04b8
Thomas-Quig <trquig1@gmail.com>
2020-10-09 00:21:50 -0500

    Automatic, description unavailable, see changes

--- src/client/client.c
+++ src/client/client.c
@@ -5,7 +5,7 @@ int main(int argc, char ** argv)
 	print_mac();
 	uint8_t _newmac[6];
 	char _newmac_pretty[13];
-	scanf("%02X:%02X:%02X:%02X:%02X:%02X",_newmac,_newmac + 1,_newmac + 2, _newmac + 3,&_newmac + 4,_newmac + 5);
+	scanf("%02hhX:%02hhX:%02hhX:%02hhX:%02hhX:%02hhX",_newmac,_newmac + 1,_newmac + 2, _newmac + 3,&_newmac + 4,_newmac + 5);
 	sprintf(_newmac_pretty,"%x:%x:%x:%x:%x:%x",_newmac[0],_newmac[1],_newmac[2],_newmac[3],_newmac[4],_newmac[5]);
 	printf("New Mac: %s\n",_newmac_pretty);
 	change_mac(_newmac);
