879917af3142fefe75d2d273ca14ab715efbaa83
Thomas-Quig <trquig1@gmail.com>
2020-10-11 15:43:43 -0500

    How tf does a linker work I swear to god

--- src/client/client.c
+++ src/client/client.c
@@ -6,7 +6,7 @@ int client_main(int argc, char ** argv)
 	printf("\n");
 	uint8_t _newmac[6];
 	char _newmac_pretty[13];
-	scanf("%x:%x:%x:%x:%x:%x",_newmac,_newmac + 1,_newmac + 2,_newmac + 3,_newmac + 4,_newmac + 5);
+	scanf("%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",_newmac,_newmac + 1,_newmac + 2,_newmac + 3,_newmac + 4,_newmac + 5);
 	sprintf(_newmac_pretty,"%x:%x:%x:%x:%x:%x",_newmac[0],_newmac[1],_newmac[2],_newmac[3],_newmac[4],_newmac[5]);
 	printf("New Mac: %s\n",_newmac_pretty);
 
