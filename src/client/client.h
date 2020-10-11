ce05b5c7526591911796c94a5a4f82c89465640e
Thomas-Quig <trquig1@gmail.com>
2020-10-11 15:59:02 -0500

    To test this, every change i make is a commit

--- src/client/client.h
+++ src/client/client.h
@@ -1,3 +1,3 @@
 #include "utils-client.h"
-
+#include "../utils/utils.h"
 int client_main(int argc, char ** argv);
--- src/server/server.h
+++ src/server/server.h
@@ -1,3 +1,3 @@
 #include "utils-server.h"
-
+#include "../utils/utils.h"
 int server_main(int argc, char ** argv);
