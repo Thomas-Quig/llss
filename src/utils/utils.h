33def9938f4ec73c74ef70019a34c56eb661029c
Thomas-Quig <trquig1@gmail.com>
2020-10-11 16:04:00 -0500

    HOLY JEEBUS

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <net/if_arp.h>
+#include <net/if.h>
+#include <assert.h>
 /**
 *	Gets the MAC Address 
 *	It does the thing, creates 12B of space on the heap
