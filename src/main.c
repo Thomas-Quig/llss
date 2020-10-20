c42a3eef19fb8bb578a80eab84cba45ebc32b822
Thomas-Quig <trquig1@gmail.com>
2020-10-19 19:00:01 -0500

    no

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include "server/server.h"
 #include "utils/utils.h"
 int main(int argc, char ** argv){
-    if(argc >= 2 && !strncmp(argv[1],"client",7) || argc == 1))
+    if(argc >= 2 && (!strncmp(argv[1],"client",7) || argc == 1))
     {
         //Run the client given argc and argv
         client_main(argc, argv);
@@ -12,7 +12,7 @@ int main(int argc, char ** argv){
         //Run the server given argc and argv
         server_main(argc, argv);
     }
-    else if(argc == 1 && (!strncmp(argv[1],"help",5) || !strncmp(argv[1],"-h",3) || !strncmp(argv[1],"--help",7))
+    else if(argc == 1 && (!strncmp(argv[1],"help",5) || !strncmp(argv[1],"-h",3) || !strncmp(argv[1],"--help",7)))
     {
         printf("usage: llss (client,server) <args>\n");
     }
