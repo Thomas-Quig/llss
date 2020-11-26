071f9713eb86a1721c44061536eb0ce39ba045ac
Thomas-Quig <trquig1@gmail.com>
2020-11-25 18:58:31 -0600

    Automatic, description unavailable, see changes

--- src/client/utils-client.c
+++ src/client/utils-client.c
@@ -12,6 +12,7 @@ ssize_t s_send(connection * conn, char * data, size_t size)
 ssize_t s_recv(connection * conn, char * data, size_t size)
 {
     dbprintf("s_recv(%p,%p,%d)\n",conn,data,size);
+    return recv(conn -> fd, data, size - 1, MSG_WAITALL);
     return recvfrom(conn -> fd, data, size, 
 			MSG_WAITALL, (struct sockaddr *) &(conn -> s_addr),&(conn ->s_len));
 }
