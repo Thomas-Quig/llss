0319407cf5938dfb4a3b96ac70b3133fa991df05
Thomas-Quig <trquig1@gmail.com>
2020-12-02 00:42:54 -0600

    Added includes.h

---
+++ src/utils/includes.h
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <pthread.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <argp.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <inttypes.h> 
+#include <ctype.h>
+
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <net/if_arp.h>
+#include <net/if.h>
+#include <arpa/inet.h> 
+
+#include <netinet/in.h>       // IPPROTO_RAW
+#include <netinet/ip.h>       // IP_MAXPACKET (which is 65535)
+#include <bits/ioctls.h>      // defines values for argument "request" of ioctl.
+#include <net/if.h>           // struct ifreq
+#include <linux/if_ether.h>   // ETH_P_ARP = 0x0806
+#include <linux/if_packet.h>  // struct sockaddr_ll (see man 7 packet)
+#include <net/ethernet.h>
+
+#include <openssl/dh.h>
+#include <openssl/bn.h>
+#include <openssl/err.h>
+#include <openssl/crypto.h>
+#include <openssl/cryptoerr.h>
+#include <libexplain/ioctl.h>
+
+
+#pragma once
