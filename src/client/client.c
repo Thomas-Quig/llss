976519d0786b9010d615d845d62c252431fc8c01
Thomas-Quig <trquig1@gmail.com>
2020-12-06 14:08:25 -0600

    Added check_file into config

--- src/client/client.c
+++ src/client/client.c
@@ -83,11 +83,11 @@ void configure(char * conf_path)
     }
     else if(conf_path == NULL)
     {
-        char * param_qs[6] = {"Verbose mode (print debug statements)","Print function calls","Shuffle MAC addresses","Encrypt messages","Cleanup arp at end of session", "Log system (debug/funccall) messages to file"};
-        int * params[6] = {&(_global_conf._VERBOSE),&(_global_conf._FUNCLIST),&(_global_conf._SHUFFLE),&(_global_conf._ENCRYPT),&(_global_conf._CLEANUP),&(_global_conf._LOG_SYS)};
+        char * param_qs[7] = {"Verbose mode (print debug statements)","Print function calls","Shuffle MAC addresses","Encrypt messages","Cleanup arp at end of session", "Log system (debug/funccall) messages to file", "Check for file when sending message"};
+        int * params[7] = {&(_global_conf._VERBOSE),&(_global_conf._FUNCLIST),&(_global_conf._SHUFFLE),&(_global_conf._ENCRYPT),&(_global_conf._CLEANUP),&(_global_conf._LOG_SYS),&(_global_conf._CHECK_FILE)};
         char _arg[8];
         int _bfr = -1;
-        for(int i = 0; i < 6;)
+        for(int i = 0; i < 7;)
         {
             _bfr = -1;
             printf("%s?: ",param_qs[i]);
