02dc1b961990be6997e7f1b5061c5b80a5e05e75
Thomas-Quig <trquig1@gmail.com>
2020-12-05 21:28:34 -0600

    h

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -193,7 +193,7 @@ char * estab_shared_secret(connection * conn, int mode)
 
     /* Do something with the shared secret */
     /* Note secret_size may be less than DH_size(mykey) */
-    printf("The shared secret is:\n");
+    printf("The shared secret is: %.32x\n",secret);
     BIO_dump_fp(stdout, secret, secret_size);
 
     return secret;
