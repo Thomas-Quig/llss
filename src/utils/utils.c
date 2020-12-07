12a24258640dc2624e9f41a718062b7edb1f6a59
Thomas-Quig <trquig1@gmail.com>
2020-12-07 17:33:09 -0600

    Timing aaaaaaaaaaa

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -25,7 +25,7 @@ ssize_t s_send(connection * conn, char * data, size_t size)
     (conn -> s_addr).sin_port = htons(conn -> port);
     (conn -> s_addr).sin_addr.s_addr = inet_addr(conn -> ip);
     //(conn -> s_addr).sin_addr.s_addr = inet_addr(conn -> ip);
-    usleep(12500);
+    usleep(20000);
     ssize_t retval = sendto(conn -> fd, (const char *)(data), size, MSG_CONFIRM,
             (const struct sockaddr *) &(conn -> s_addr),conn -> s_len);
     return retval;
