098a3dea8ff6a3a606e484e727e66bb72dca3604
Thomas-Quig <trquig1@gmail.com>
2020-12-08 00:13:22 -0600

    what the hell

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -26,7 +26,7 @@ ssize_t s_send(connection * conn, char * data, size_t size)
     (conn -> s_addr).sin_addr.s_addr = inet_addr(conn -> ip);
     //(conn -> s_addr).sin_addr.s_addr = inet_addr(conn -> ip);
     //if(conn -> mode == __CLIENT_SEND)
-    usleep(20000);
+    usleep(100000);
     ssize_t retval = sendto(conn -> fd, (const char *)(data), size, MSG_CONFIRM,
             (const struct sockaddr *) &(conn -> s_addr),conn -> s_len);
     return retval;
